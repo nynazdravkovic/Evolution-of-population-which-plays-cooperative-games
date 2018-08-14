b8962b003e162316df984969d859cab1705ef2fd
nynazdravkovic <nyna.zdravkovic@gmail.com>
2018-08-14 17:35:29 +0200

    dobar bar, ograniceno na 24 str

--- genalg.py
+++ genalg.py
@@ -20,13 +20,14 @@ brojJedinki=100
 brojCiklusa=100
 koeficijentMutacije=0.01
 koeficijentKrosovera=0.05
-brojGeneracija=5000
+brojGeneracija=4000
 cc=3
 cd=0
 dc=5
 dd=1
 razliciteStrategije=64
 matrica= numpy.zeros([brojGeneracija,64], dtype=int)
+
 
     
 
@@ -184,6 +185,60 @@ def genetskiAlgoritam():
             for i in range (0,64):
                 if populacija[k]==i:
                     matrica[t][i]=matrica[t][i]+1
+        matrica[t][0]+=matrica[t][1]+matrica[t][2]+matrica[t][3]+matrica[t][4]+matrica[t][5]+matrica[t][6]+matrica[t][8]+matrica[t][9]+matrica[t][11]+matrica[t][12]+matrica[t][13]
+        matrica[t][16]+=matrica[t][18]+matrica[t][19]
+        matrica[t][20]+=matrica[t][22]
+        matrica[t][21]+=matrica[t][23]
+        matrica[t][52]+=matrica[t][53]+matrica[t][55]
+        matrica[t][24]+=matrica[t][27]
+        matrica[t][48]+=matrica[t][49]
+        matrica[t][36]+=matrica[t][39]
+        matrica[t][63]+=matrica[t][62]+matrica[t][61]+matrica[t][60]+matrica[t][59]+matrica[t][58]+matrica[t][57]+matrica[t][55]+matrica[t][54]+matrica[t][52]+matrica[t][51]+matrica[t][50]
+        matrica[t][47]+=matrica[t][45]+matrica[t][44]
+        matrica[t][43]+=matrica[t][41]
+        matrica[t][42]+=matrica[t][40]
+        matrica[t][11]+=matrica[t][10]+matrica[t][8]
+        matrica[t][39]+=matrica[t][36]
+        matrica[t][15]+=matrica[t][14]
+        matrica[t][28]+=matrica[t][24]
+        matrica[t][1]=0
+        matrica[t][2]=0       
+        matrica[t][3]=0
+        matrica[t][4]=0
+        matrica[t][5]=0
+        matrica[t][6]=0
+        matrica[t][8]=0
+        matrica[t][9]=0
+        matrica[t][11]=0
+        matrica[t][12]=0
+        matrica[t][13]=0
+        matrica[t][18]=0
+        matrica[t][19]=0
+        matrica[t][22]=0
+        matrica[t][23]=0
+        matrica[t][53]=0
+        matrica[t][55]=0
+        matrica[t][27]=0
+        matrica[t][49]=0
+        matrica[t][39]=0
+        matrica[t][24]=0
+        matrica[t][14]=0
+        matrica[t][36]=0
+        matrica[t][10]=0
+        matrica[t][40]=0
+        matrica[t][62]=0
+        matrica[t][61]=0
+        matrica[t][60]=0
+        matrica[t][59]=0
+        matrica[t][58]=0
+        matrica[t][57]=0
+        matrica[t][55]=0
+        matrica[t][54]=0
+        matrica[t][53]=0
+        matrica[t][52]=0
+        matrica[t][51]=0
+        matrica[t][50]=0
+        matrica[t][41]=0
     putanja=(r'C:\Users\nina\Desktop\projekat2018\histogrami\histogram')
     b=putanja + str(t) + '.jpg'
     ''.join(b)
@@ -193,13 +248,15 @@ def genetskiAlgoritam():
     m=matrica.transpose()
     for i in range (64):
         plt.plot(vreme,m[i])
+        axes = plt.gca()
+        axes.set_ylim([0,500])
         putanja=(r'C:\Users\nina\Desktop\projekat2018\svaka\grafik')
         b=putanja + str(i) + '.jpg'
         ''.join(b)
         plt.savefig(b)
     plt.show()
     return nizSrednjihPoena, matrica
-         
+
 
 def sve():
     o=list(range(0,64))
@@ -215,13 +272,10 @@ def sve():
         srednja.append(srednjaVrednost)
         plt.plot(vreme, srednjaVrednost)  
     plt.show()
-    plt.bar(o,matrica)
-    numpy.rot90(srednja)
-    m=matrica.transpose()
-    for i in range (brojGeneracija):
-        for k in range (64):
-            m[k][i]=m[k][i]
-    plt.show(m)
+    #m=matrica.transpose()
+    for x in range (brojGeneracija):
+        plt.bar(o,matrica[3999])
+    plt.show()
     
     
     
